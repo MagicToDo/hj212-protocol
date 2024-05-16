e7ad9e174277a293f5c715ac356015464b4d12c2
MagicLiu <liuyong946@sina.cn>
2024-05-16 11:05:40 +0800

    refactor(reans.去除冗余不需要的定时任务业务代码): 去除冗余业务代码

--- example/src/main/java/com/github/guocay/hj212/business/trans/mapper/DataAirMapper.java
+++
@@ -1,7 +0,0 @@
-package com.github.guocay.hj212.business.trans.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.guocay.hj212.business.trans.bean.DataAir;
-
-public interface DataAirMapper extends BaseMapper<DataAir> {
-}
--- example/src/main/java/com/github/guocay/hj212/business/trans/mapper/DataVocMapper.java
+++
@@ -1,7 +0,0 @@
-package com.github.guocay.hj212.business.trans.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.guocay.hj212.business.trans.bean.DataVoc;
-
-public interface DataVocMapper extends BaseMapper<DataVoc> {
-}
